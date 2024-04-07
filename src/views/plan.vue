b7ea76395b4d8acbf77fd8a36dcee75d33bb2fa2
Xiaoming <coderlxm@gmial.com>
2024-04-07 18:03:30 +0800

    plan page

--- src/views/plan.vue
+++ src/views/plan.vue
@@ -1,4 +1,26 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+const plan = ref()
+const planOptions = [
+  {
+    label: '全部',
+    value: 'all'
+  },
+  {
+    label: '按周期',
+    value: 'cycle'
+  },
+  {
+    label: '按流量',
+    value: 'traffic'
+  },
+]
+</script>
 <template>
-  <div>计划</div>
+  <div>
+    <div class="font-size-30px mb-5px">选择最适合您的计划</div>
+    <n-radio-group v-model:value="plan" name="radiobuttongroup1">
+      <n-radio-button v-for="item in planOptions" :key="item.value" :value="item.value" :label="item.label" />
+    </n-radio-group>
+  </div>
 </template>
