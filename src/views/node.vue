2f64064d4fd7a7ed27c4550db571c694ca33c8a0
Xiaoming <coderlxm@gmial.com>
2024-04-07 18:10:08 +0800

    node

--- src/views/node.vue
+++ src/views/node.vue
@@ -1,4 +1,8 @@
-<script setup></script>
+<script setup>
+
+</script>
 <template>
-  <div>节点</div>
+  <n-alert type="info">
+    没有可用节点，如果您未订阅或已过期请 <router-link class="font-700" :to="{ name: 'plan' }">订阅</router-link>
+  </n-alert>
 </template>
