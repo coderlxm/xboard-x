04eec783b2d951756c2bbf615955aa136cc568da
Xiaoming <coderlxm@gmial.com>
2024-04-20 15:40:35 +0800

    resolve tag title

--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,22 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes
 })
-router.beforeEach((to) => {
-  const token = localStorage.getItem('token')
-  if ((to.name !== 'login' && to.name !== 'register' && to.name !== 'forgetpassword') && !token) {
-    return { name: 'login' }
+router.beforeEach((to, from, next) => {
+  // 检查用户是否已登录，如果尝试访问非公开路由则重定向到登录页面
+  const token = localStorage.getItem('token');
+  const publicPages = ['login', 'register', 'forgetpassword']; // 不需要身份验证的页面路由名称
+
+  const authRequired = !publicPages.includes(to.name);
+  if (authRequired && !token) {
+    return next({ name: 'login' }); // 重定向到登录页面
   }
+
+  // 如果上面的验证通过了，则设置页面标题
+  const pageTitle = to.meta.title;
+  if (pageTitle) {
+    document.title = pageTitle + ' | 小明云加速 Pro';
+  }
+
+  next(); // 确保调用 next()！
 })
 export default router
--- src/router/routes.js
+++ src/router/routes.js
@@ -3,7 +3,8 @@ const routes = [
     name: 'login',
     path: '/login',
     meta: {
-      isLogin: true
+      isLogin: true,
+      title: '登录'
     },
     component: () => import('@/views/loginPage.vue')
   },
@@ -11,7 +12,8 @@ const routes = [
     name: 'register',
     path: '/register',
     meta: {
-      isLogin: true
+      isLogin: true,
+      title: '注册'
     },
     component: () => import('@/views/loginPage.vue')
   },
@@ -19,7 +21,8 @@ const routes = [
     name: 'forgetpassword',
     path: '/forgetpassword',
     meta: {
-      isLogin: true
+      isLogin: true,
+      title: '忘记密码'
     },
     component: () => import('@/views/loginPage.vue')
   },
@@ -31,46 +34,73 @@ const routes = [
       {
         name: 'traffic',
         path: '/traffic',
+        meta: {
+          title: '流量明细'
+        },
         component: () => import('@/views/traffic.vue')
       },
       {
         name: 'ticket',
         path: '/ticket',
+        meta: {
+          title: '我的工单'
+        },
         component: () => import('@/views/ticket.vue')
       },
       {
         name: 'profile',
         path: '/profile',
+        meta: {
+          title: '个人中心'
+        },
         component: () => import('@/views/profile.vue')
       },
       {
         name: 'node',
         path: '/node',
+        meta: {
+          title: '节点状态'
+        },
         component: () => import('@/views/node.vue')
       },
       {
         name: 'plan',
         path: '/plan',
+        meta: {
+          title: '购买订阅'
+        },
         component: () => import('@/views/plan.vue')
       },
       {
         name: 'invite',
         path: '/invite',
+        meta: {
+          title: '我的邀请'
+        },
         component: () => import('@/views/invite.vue')
       },
       {
         name: 'order',
         path: '/order',
+        meta: {
+          title: '我的订单'
+        },
         component: () => import('@/views/order.vue')
       },
       {
         name: 'knowledge',
         path: '/knowledge',
+        meta: {
+          title: '使用文档'
+        },
         component: () => import('@/views/knowledge.vue')
       },
       {
         name: 'dashboard',
         path: '/dashboard',
+        meta: {
+          title: '仪表盘'
+        },
         component: () => import('@/views/dashboard.vue')
       },
     ]
